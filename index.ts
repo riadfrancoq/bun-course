cef3a77a9538da9bbf1b720edabc4fd8a2c10290
riadfrancoq <campusricardo2023@gmail.com>
2024-03-12 16:18:27 -0500

    feat: :sparkles: bun server finished

--- index.ts
+++ index.ts
@@ -1,9 +1,36 @@
-const z = 10;
+import figlet from "figlet";
 
-const read = (text: string) => {
-    return text;
-}
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/"){ 
+        const body = figlet.textSync("video");
+        return new Response(body);
+            
+        };
+        if (url.pathname === "/about") {
+            return new Response("About me!");
+        }
 
-const message = read("Hello world!");
+        if (url.pathname === "/contact") {
+            return new Response(" Contact Us!")
+        }
 
-console.log(message);
+        // handle Error
+        if (url.pathname === "/feed") {
+            throw new Error("Could not fetch feed");
+        }
+        return new Response('404!');
+    },
+    error(request) {
+        return new Response(`<pre> ${request} \n ${request.stack} </pre>`, {
+            headers: {
+                'Content-Type': 'text/html'
+            }
+        }
+        );
+    },
+});
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
