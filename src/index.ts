c889dbdfa065ff63258eac00dc4a7fe7547a28c0
riadfrancoq <campusricardo2023@gmail.com>
2024-03-12 16:48:35 -0500

    feat: :sparkles: finished elysia rest api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").get("/post/:id", ({params: {id}})=>  {
+  return {id};
+}).post('/post', ({body, set})=> {
+  set.status = 201;
+  return body;
+})
+.get("/track/*", ()=> {return "Track Route"})
+.get('/tracks', ()=> {
+
+    return new Response(JSON.stringify({
+      "tracks": [
+        'Dancing Feet',
+        'Sam I',
+        'Animals'
+      ]
+    }),
+    {
+      headers: {
+        'Content-Type': "application/json"
+      }
+    });
+})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
